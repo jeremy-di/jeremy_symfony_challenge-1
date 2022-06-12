b204600306d2c5761fd8c1bcde83cd2e0cd50878
jeremy-di <moinet.jeremy@gmail.com>
2022-06-13 00:02:03 +0200

    Dernieres modifications et correction orthographe

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -36,6 +36,7 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $userRepository->add($user, true);
+            
 
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
